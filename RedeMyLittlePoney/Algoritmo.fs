8327ae2c16caac63340c00c044e9cb5cc23c1c42
Yan Paulo <yanpaulo@hotmail.com>
2018-09-16 14:51:00 -0300

    XOR.

--- RedeMyLittlePoney/Algoritmo.fs
+++ RedeMyLittlePoney/Algoritmo.fs
@@ -291,3 +291,43 @@ module Algoritmo =
         let neuronios = [7 .. 10]
 
         algoritmoCSV db classes 10 neuronios taxas
+
+    let algoritmoXor () =
+        printfn "XOR"
+        
+        let range min max n =
+            let range = max - min
+            n * range + min
+        
+        let mapV (x, y) = 
+            vector [x; y]
+
+        let x1 = Random.doubles 50 |> Seq.map (range 0.0 0.5)
+        let y1 = Random.doubles 50 |> Seq.map (range 0.0 0.5)
+        let q1 = Seq.zip x1 y1 |> Seq.map mapV |> List.ofSeq
+
+        let x2 = Random.doubles 50 |> Seq.map (range 0.5 1.0)
+        let y2 = Random.doubles 50 |> Seq.map (range 0.0 0.5)
+        let q2 = Seq.zip x2 y2 |> Seq.map mapV |> List.ofSeq
+
+        let x3 = Random.doubles 50 |> Seq.map (range 0.0 0.5)
+        let y3 = Random.doubles 50 |> Seq.map (range 0.5 1.0)
+        let q3 = Seq.zip x3 y3 |> Seq.map mapV |> List.ofSeq
+        
+        let x4 = Random.doubles 50 |> Seq.map (range 0.5 1.0)
+        let y4 = Random.doubles 50 |> Seq.map (range 0.5 1.0)
+        let q4 = Seq.zip x4 y4 |> Seq.map mapV |> List.ofSeq
+
+        let classes = [ vector [1.0; 0.0]; vector [0.0; 1.0]]
+
+        let mapClass index c =
+            {X = c; Y = classes.[index]}
+
+        let classe1 = q1 @ q4 |> List.map (mapClass 0)
+        let classe2 = q2 @ q3 |> List.map (mapClass 1)
+        let dados = classe1 @ classe2
+        
+        let neuronios = [4 .. 10]
+        let taxas = [0.1 .. 0.1 .. 0.5]
+
+        algoritmo dados classes neuronios taxas
