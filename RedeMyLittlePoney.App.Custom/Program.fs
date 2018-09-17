950bc1cfd45b9ffccd2e57c6468eeca5469802ff
Yan Paulo <yanpaulo@hotmail.com>
2018-09-17 02:02:29 -0300

    CustomApp.

---
+++ RedeMyLittlePoney.App.Custom/Program.fs
@@ -0,0 +1,7 @@
+﻿// Saiba mais sobre F# em http://fsharp.org
+// Veja o projeto 'F# Tutorial' para obter mais ajuda.
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // retornar um código de saída inteiro
