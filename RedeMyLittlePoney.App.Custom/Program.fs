6ba64cd38a00a2dce05123ca07445e01422c7ad9
Yan Paulo <yanpaulo@hotmail.com>
2018-09-17 07:41:15 -0300

    Chamada de algoritmos no Custom.

--- RedeMyLittlePoney.App.Custom/Program.fs
+++ RedeMyLittlePoney.App.Custom/Program.fs
@@ -1,7 +1,12 @@
-﻿// Saiba mais sobre F# em http://fsharp.org
-// Veja o projeto 'F# Tutorial' para obter mais ajuda.
+﻿namespace RedeMyLittlePoney
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // retornar um código de saída inteiro
+// Saiba mais sobre F# em http://fsharp.org
+// Veja o projeto 'F# Tutorial' para obter mais ajuda.
+module Application =
+    [<EntryPoint>]
+    let main argv = 
+        printfn "%A" (Algoritmo.algoritmoIris())
+        printfn "%A" (Algoritmo.algoritmoColuna())
+        printfn "%A" (Algoritmo.algoritmoDermatologia())
+        printfn "%A" (Algoritmo.algoritmoCancer())
+        0 // retornar um código de saída inteiro
